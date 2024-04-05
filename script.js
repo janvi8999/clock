0dfc55e227ad66ab7b016804839756858c2d0c2f
janvi8999 <166148296+janvi8999@users.noreply.github.com>
2024-04-05 21:49:45 +0530

    clock
    clock

---
+++ script.js
@@ -0,0 +1,61 @@
+// console.log("I am student");
+
+// var name=prompt("I am taking input");
+// console.log(name);
+
+// for(let i=1;i<=10;i++){
+//     console.log(i*2);
+// }
+
+// let n=1;
+// while(n<=10){
+//     console.log(n);
+//     // n++;
+// }
+
+
+// let n=prompt("Given me number");
+// if(n%2==0);{
+//     console.log("I am even");
+
+// }
+//  else{
+//     console.log("I am Odd");
+
+
+
+// let marks=prompt("Enter your marks");
+// if(marks>80 && marks<=90){
+//     console.log("A Grade");
+
+// }
+// else if(marks>70 && marks<=80){
+//  console.log("B Grade");
+    
+// }
+// else if(marks>60 && marks<=70){
+//     console.log("C Grade");
+       
+//    }
+//    else{
+//     console.log("D Grade")
+//    }
+
+// 
+    //    guess the movie
+
+
+//     const favMovie="avengers";
+//     let guess=prompt("Guess my favourite");
+
+//     while((guess!=favMovie)&& (guess!="quit")){
+//         guess=prompt("Wrong Answer!! Please try again!!");  
+//     }
+//     if(guess==favMovie){
+//         console.log("Congrats Buddy!!")
+//     }
+//     else{ console.log("You Quited");
+// }
+
+
+    
